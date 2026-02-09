6855381d7a2e20701c5989226a900793824b591d
avarshvir <avarshvir@gmail.com>
2026-02-09 21:55:39 +0530

    A. Soldier and Bananas

---
+++ 800-rating/A. Soldier and Bananas/solution.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int k, n, w;
+    int total_cost = 0;
+
+    cin >> k >> n >> w;
+
+    for (int i = 1; i <= w; i++) {
+        total_cost += i * k;
+    }
+
+    if (total_cost > n) {
+        cout << total_cost - n;
+    } else {
+        cout << 0;
+    }
+
+    return 0;
+}
