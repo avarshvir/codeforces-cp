59911bf76de137d1b9a7ab953e4a88efd3d73366
avarshvir <avarshvir@gmail.com>
2026-02-26 18:45:14 +0530

    110 A Solution

---
+++ 800-rating/A. Nearly Lucky Number/solution.cpp
@@ -0,0 +1,19 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+    long long n;
+    cin >> n;
+    string s = to_string(n);
+    int count = 0;
+    for(char c : s){
+        if(c == '4' || c == '7') count++;
+    }
+
+    if(count > 0 && (count == 4 || count == 7))
+        cout << "YES\n";
+    else
+        cout << "NO\n";
+
+    return 0;
+}
