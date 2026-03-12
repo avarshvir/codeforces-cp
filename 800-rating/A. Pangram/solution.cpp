a762432f0ca77eec3cc9c219c7a4426627ee17d8
avarshvir <avarshvir@gmail.com>
2026-03-12 22:13:46 +0530

    A. Pangram solution

---
+++ 800-rating/A. Pangram/solution.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <set>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;
+    cin >> n;
+    string s;
+    cin >> s;
+
+    set<char> letters;
+    for(int i = 0; i < n; i++){
+        letters.insert(tolower(s[i]));
+    }
+
+    if(letters.size() == 26){
+        cout << "YES" << endl;
+    } else {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
