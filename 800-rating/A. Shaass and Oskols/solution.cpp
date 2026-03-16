71158e9f0c37346e6a8afacd19f6a6d834fe5516
avarshvir <avarshvir@gmail.com>
2026-03-16 21:45:51 +0530

    solution

---
+++ 800-rating/A. Shaass and Oskols/solution.cpp
@@ -0,0 +1,34 @@
+#include<iostream>
+using namespace std;
+int main(){
+    
+    int wires;
+    cin >> wires;
+    vector<int> birds(wires);
+    for(int i = 0; i < wires; i++){
+        cin >> birds[i];
+    }
+    
+    int shots;
+    cin >> shots;
+    vector<pair<int,int>> shotInfo(shots);
+    for(int i = 0; i < shots; i++){
+        int shotWireNo, shotBirdNo;
+        cin >> shotInfo[i].first >> shotInfo[i].second;
+    }
+    
+    for(int i = 0; i < shots; i++) {
+        int wire = shotInfo[i].first - 1;  
+        int pos = shotInfo[i].second;
+
+        if(wire > 0) birds[wire - 1] += pos - 1;
+
+        if(wire < wires - 1) birds[wire + 1] += birds[wire] - pos;
+
+        birds[wire] = 0;
+    }
+
+    for(int i = 0; i < wires; i++) {
+        cout << birds[i] << endl;
+    }
+}
