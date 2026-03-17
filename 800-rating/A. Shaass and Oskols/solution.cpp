2d971dd18d4f8939eec99878222639aa2511952b
avarshvir <avarshvir@gmail.com>
2026-03-17 22:29:56 +0530

    A shaass and oskols

--- 800-rating/A. Shaass and Oskols/solution.cpp
+++ 800-rating/A. Shaass and Oskols/solution.cpp
@@ -1,10 +1,12 @@
 #include<iostream>
 #include<vector>
 using namespace std;
+
 int main(){
     
     int wires;
     cin >> wires;
+    
     vector<int> birds(wires);
     for(int i = 0; i < wires; i++){
         cin >> birds[i];
@@ -12,9 +14,9 @@ int main(){
     
     int shots;
     cin >> shots;
+    
     vector<pair<int,int>> shotInfo(shots);
     for(int i = 0; i < shots; i++){
-        int shotWireNo, shotBirdNo;
         cin >> shotInfo[i].first >> shotInfo[i].second;
     }
     
@@ -22,9 +24,14 @@ int main(){
         int wire = shotInfo[i].first - 1;  
         int pos = shotInfo[i].second;
 
-        if(wire > 0) birds[wire - 1] += pos - 1;
+        int left = pos - 1;
+        int right = birds[wire] - pos;
 
-        if(wire < wires - 1) birds[wire + 1] += birds[wire] - pos;
+        if(wire > 0) 
+            birds[wire - 1] += left;
+
+        if(wire < wires - 1) 
+            birds[wire + 1] += right;
 
         birds[wire] = 0;
     }
@@ -32,4 +39,6 @@ int main(){
     for(int i = 0; i < wires; i++) {
         cout << birds[i] << endl;
     }
+
+    return 0;
 }
